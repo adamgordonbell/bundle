5c3972140d6a3369ba9627ecfb76a5df3696c3ff
Adam Bell <agbell@gmail.com>
2016-08-13 17:02:17 +0000

    working greedy algorithm

--- build.sbt
+++ build.sbt
@@ -1,3 +1,4 @@
 scalaVersion := "2.11.7"
 
 libraryDependencies += "org.scalatest" % "scalatest_2.11" % "2.2.5" % "test"
+libraryDependencies += "io.github.nicolasstucki" %% "multisets" % "0.3"
--- src/test/scala/Tests.scala
+++ src/test/scala/Tests.scala
@@ -1,19 +1,41 @@
 import org.scalatest.{Matchers, FunSuite}
 
 class Tests extends FunSuite with Matchers {
-  val products: List[Product] = List(
-    Product(1,"Apple"),
-    Product(2,"Orange"),
-    Product(3, "Bread")
-  )
-  val pricing: List[Price] = products.map(Price(_,1))
+  val apple = Product(1,"Apple")
+  val bread = Product(2,"Bread")
+  val butter = Product(3,"Butter")
 
-  val bundles = List(
-    Bundle(products, 20),
-    Bundle(products.take(2),20)
+  val products: List[Product] = List(
+    apple,
+    bread,
+    butter
   )
+  val pricing = List(
+    Price(apple, 1.99),
+    Price(bread, 3.99),
+    Price(butter, 2.50)
+  )
+
+  //Two Apples are 2.15
+  val twoApples = Bundle(List(
+      Price(apple,2.15),
+      Price(apple,0)
+    ))
+
+  //Buy Bread and a stick of butter and the second stick is free
+  val freeStickOfButter = Bundle(List(
+      Price(bread, 3.99),
+      Price(butter, 2.50),
+      Price(butter, 0)
+  ))
+
+  val bundles = List(twoApples,freeStickOfButter)
 
   test("no bundles") {
-    BundlePricing(pricing, List.empty).price(products) shouldEqual 3
+    BundlePricing(pricing, List.empty).price(products) shouldEqual 8.48
+  }
+
+  test("two apples") {
+    BundlePricing(pricing, bundles).price(List(apple, apple)) shouldEqual 2.15
   }
 }
