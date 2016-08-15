7c384f1d5272c35006233dea42379cd18747a46b
Adam Bell <agbell@gmail.com>
2016-08-15 12:59:39 +0000

    improve read ability

--- src/main/scala/PricingService.scala
+++ src/main/scala/PricingService.scala
@@ -19,27 +19,35 @@ case class PricingService(basePrices: Map[SKU, BigDecimal], discounts: List[Disc
   }
 
   def applyDiscount(remainingItems: Bag[SKU]): BigDecimal = {
-    def getCombinationWithLowestPrice(list: List[Discount]): BigDecimal = {
-      def subtractDiscount(items: Bag[SKU], discount: Discount): Bag[SKU] = {
-        items.diff(discount.items)
-      }
-      val stepOneResult = list.map(x => (subtractDiscount(remainingItems, x), x))
-      val stepNResult = stepOneResult.map(x => x._2.discountPrice + applyDiscount(x._1))
-      stepNResult.sortWith(_ < _).head
-    }
     def apply: BigDecimal = {
-      def basePriceSum(products: Bag[SKU]): BigDecimal = {
-        def default() : BigDecimal = 0
-        products.map(basePrices.getOrElse(_,default)).sum
-      }
-      def getAllDiscountsThatApply(products: Bag[SKU]): List[Discount] = {
-        discounts.filter(bundle => bundle.items.intersect(products) == bundle.items)
-      }
       getAllDiscountsThatApply(remainingItems) match {
         case Nil => basePriceSum(remainingItems)
-        case list => getCombinationWithLowestPrice(list)
+        case list => getCombinationWithLowestPrice(remainingItems, list)
       }
     }
     cache.getOrElseUpdate(remainingItems.toString, apply)
+  }
+
+  private def getAllDiscountsThatApply(products: Bag[SKU]): List[Discount] = {
+    discounts.filter(bundle => bundle.items.intersect(products) == bundle.items)
+  }
+
+  private def basePriceSum(products: Bag[SKU]): BigDecimal = {
+    def default() : BigDecimal = 0
+    products.map(basePrices.getOrElse(_,default)).sum
+  }
+
+  //Check all combinations starting with each discount via recursion and return the lowest
+  private def getCombinationWithLowestPrice(remainingItems : Bag[SKU], list: List[Discount]): BigDecimal = {
+    def subtractDiscount(items: Bag[SKU], discount: Discount): Bag[SKU] = {
+      items.diff(discount.items)
+    }
+    val stepOneResult: List[(Bag[SKU], Discount)] = {
+      list.map(x => (subtractDiscount(remainingItems, x), x))
+    }
+    val stepNResult: List[BigDecimal] = {
+      stepOneResult.map(x => x._2.discountPrice + applyDiscount(x._1))
+    }
+    stepNResult.sortWith(_ < _).head
   }
 }
